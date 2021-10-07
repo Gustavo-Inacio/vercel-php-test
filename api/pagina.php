b183a50835a6f5878372ea5106a3685cc0a51cb3
Gustavo Inacio <gutiinacio@gmai.com>
2021-10-07 20:44:37 -0300

    commit

---
+++ api/pagina.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./styles.css">
+        <link rel="stylesheet" href="./styles.css">
+        <link rel="stylesheet" href="../estilo.css">
+        <link rel="stylesheet" href="./estilo2.css">
+        <script src="main.js" defer></script>
+        <title>Document</title>
+    </head>
+    <body>
+<?php
+    echo '
+    
+    pagina
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/ATileEqltGY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <h1>Meu texto</h1>
+   
+  
+    '
+?>
+ </body>
+ </html>
