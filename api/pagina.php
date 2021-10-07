5115cb9e776fafa957995ac15e7f3476e9081141
Gustavo Inacio <gutiinacio@gmai.com>
2021-10-07 19:03:56 -0300

    a

--- api/pagina.php
+++ api/pagina.php
@@ -4,7 +4,10 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./styles.css">
         <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="../estilo.css">
+        <link rel="stylesheet" href="./estilo2.css">
         <script src="main.js" defer></script>
         <title>Document</title>
     </head>
