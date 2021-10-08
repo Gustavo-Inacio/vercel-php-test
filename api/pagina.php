935708d681e234df07c50e00226e526beeb169aa
Gustavo Inacio <gutiinacio@gmai.com>
2021-10-07 21:01:49 -0300

    as

--- api/pagina.php
+++ api/pagina.php
@@ -1,6 +1,12 @@
-<?php
-
-header("location: ../public/Home/home.html")
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="../public/Home/home.html">ir para home</a>
+</body>
+</html>
